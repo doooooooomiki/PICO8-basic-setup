9a0c96f46b895479d9852e6dc1968643ecb57cae
Dominic <dominic.kirste@gmail.com>
2022-10-05 19:29:48 +0200

    added ship movement on x axis

--- main.lua
+++ main.lua
@@ -1,7 +1,38 @@
-function _draw()
-  circfill(64, 64, 24, 7)
+function _init()
+  Screen = {}
+  Screen.minX = 0
+  Screen.maxX = 128
+  Screen.background = 1
+
+  Ship = {}
+  Ship.x = 60
+  Ship.y = 112
+  Ship.speed = 3
+
+  cls(Screen.background)
 end
 
 function _update()
-  cls()
+  cls(Screen.background)
+
+  if btn(0) then
+    Ship.x = Ship.x - Ship.speed
+  end
+
+  if btn(1) then
+    Ship.x = Ship.x + Ship.speed
+  end
+
+  if Ship.x >= Screen.maxX - 8 then
+    Ship.x = 120
+  end
+
+  if Ship.x <= Screen.minX then
+    Ship.x = 0
+  end
+
+end
+
+function _draw()
+  spr(001, Ship.x, Ship.y)
 end
