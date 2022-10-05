2e318af01eba8250c29b5fbe3c585982184e5164
Dominic <dominic.kirste@gmail.com>
2022-10-05 21:57:03 +0200

    added ship accel and friction

--- main.lua
+++ main.lua
@@ -4,10 +4,51 @@ function _init()
   Screen.maxX = 128
   Screen.background = 1
 
-  Ship = {}
-  Ship.x = 60
-  Ship.y = 112
-  Ship.speed = 3
+  Ship = {
+
+    -- position
+    x = 60,
+    y = 112,
+
+    -- movement
+    control = false,
+    vx = 0,
+    vy = 0,
+    accel = 0.25,
+    friction = 0.1,
+    max_speed = 2.5,
+
+    -- health
+    max_health = 3,
+
+    -- projectile
+    projectile_speed = -4,
+    projectile_kickback = 1.4,
+
+    handle_input = function(self)
+      local vx, accel, friction, max_speed = self.vx, self.accel, self.friction, self.max_speed
+      if btn(0) then
+        vx = vx > 0 and -accel or vx - accel
+      elseif btn(1) then
+        vx = vx < 0 and accel or vx + accel
+      elseif vx != 0 then
+        vx += min(abs(vx), friction) * -sgn(vx)
+      end
+
+      self.vx = mid(-max_speed, vx, max_speed)
+
+    end,
+
+    update = function(self)
+      self:handle_input()
+      self.x = self.x + self.vx
+    end,
+
+    draw = function(self)
+      spr(001, self.x, self.y)
+    end
+
+  }
 
   cls(Screen.background)
 end
@@ -15,24 +56,10 @@ end
 function _update()
   cls(Screen.background)
 
-  if btn(0) then
-    Ship.x = Ship.x - Ship.speed
-  end
-
-  if btn(1) then
-    Ship.x = Ship.x + Ship.speed
-  end
-
-  if Ship.x >= Screen.maxX - 8 then
-    Ship.x = 120
-  end
-
-  if Ship.x <= Screen.minX then
-    Ship.x = 0
-  end
+  Ship:update()
 
 end
 
 function _draw()
-  spr(001, Ship.x, Ship.y)
+  Ship:draw()
 end
